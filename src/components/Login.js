079efbce0bf9662a630e1c3d9b0da2dfc1e2b67d
AliHaggag11 <Ali7aggag@gmail.com>
2023-10-26 01:53:37 +0300

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
+import Logo from "../Assets/sentri-logo.png"; 
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    showPassword: false,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleToggle = () => {
+    setFormData({ ...formData, showPassword: !formData.showPassword });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  };
+
+  return (
+    <div className="main-container">
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-heading">
+          <img src={Logo} className="logo" alt="Logo" />
+        </div>
+
+        <div className="form-container">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-input"
+            placeholder="Enter your email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+
+        <div className="form-container">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <label htmlFor="reset" className="reset">
+            <a href="/">Reset password</a>
+          </label>
+          <div className="password">
+            <input
+              type={formData.showPassword ? "text" : "password"}
+              className={`form-input ${!formData.password && "form-input-error"}`}
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+              style={{
+                fontFamily: 'IBM Plex Mono', 
+                fontSize: '16px',
+                lineHeight: '22px'
+              }}
+            />
+            <span className="password-span" onClick={handleToggle}>
+              {formData.showPassword ? <BsEyeSlash /> : <BsEye />}
+            </span>
+          </div>
+        </div>
+
+        <button className="btn" type="submit">
+          Log in
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
+
+
+// "This is a React component for a user login form. It manages
+//  the input fields for email and password, with state hooks. The
+//   eye icon toggles password visibility. Upon form submission, it 
+//   currently lacks actual login logic. The component uses external 
+//   resources like the react-icons library for icons and an image for 
+//   the logo. It's designed for flexibility and readability, with optimized 
+//   state management. Styling and additional functionality can be added as needed."
